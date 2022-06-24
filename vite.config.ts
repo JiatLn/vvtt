fa17de3a5dba8a437ad2edaedf0c62301b935786
林佳涛 <lne128gene980@163.com>
2022-06-24 09:28:06 +0800

    chore: update alias

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ export default ({ mode }) => {
   return defineConfig({
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@/': `${path.resolve(__dirname, 'src')}/`,
+        '~/': `${path.resolve(__dirname, 'src')}/`,
       },
     },
     plugins: [
