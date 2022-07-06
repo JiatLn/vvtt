4de7dcf438fff4ee09eb4b6a172f3984be9d0bad
cx33 <lne128gene980@163.com>
2022-07-06 22:53:01 +0800

    update: improve home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,12 +7,12 @@ const userStore = useUserStore()
 <template>
   <div h-full w-full flex="c col">
     <div flex items-start gap-2>
-      <TheBox title="unocss">
-        <div text-brand-primary uppercase>
-          Home Page
+      <TheBox title="UnoCSS">
+        <div text-brand-primary>
+          Hello, <span font-semibold>VVTT</span>
         </div>
       </TheBox>
-      <TheBox title="Vue-router">
+      <TheBox title="Vue Router">
         <div flex justify-between space-x-4>
           <router-link btn :to="{ name: 'Login' }">
             Login
@@ -22,7 +22,7 @@ const userStore = useUserStore()
           </router-link>
         </div>
       </TheBox>
-      <TheBox title="iconify">
+      <TheBox title="Iconify">
         <div flex-c gap-12px text-xl>
           <div i-flat-color-icons:home />
           <a i-akar-icons:github-fill cursor-pointer color-gray-600 hover:color-brand-primary href="https://github.com/JiatLn/vvtt" target="_blank" />
@@ -33,7 +33,7 @@ const userStore = useUserStore()
           isLogin: <span font-bold>{{ userStore.isLogin }}</span>
         </div>
       </TheBox>
-      <TheBox title="TSX support" />
+      <TheBox title="TSX Support" />
     </div>
     <p mt-8 text-gray-400>
       This is a demo page, you can clear it and start coding~ :)
