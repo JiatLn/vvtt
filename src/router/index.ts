a828f9839ee921f02fc29665782009e65d2d0fb3
林佳涛 <lne128gene980@163.com>
2022-07-20 17:02:12 +0800

    fix: vite2 merge to vite3

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes: RouteRecordRaw[] = []
 
-const modules = import.meta.globEager('./modules/*.ts')
+const modules = import.meta.glob('./modules/*.ts', { eager: true }) as Record<string, any>
 
 for (const path in modules)
   routes.push(...modules[path].default)
